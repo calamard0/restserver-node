dc49bebc405204710250f0ce831134b0864a1375
Fruchart Juan Alberto <jfruchart@uade.edu.ar>
2020-05-26 14:14:47 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,6 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://calamard0:wgUpkuz4iAPysfsx@cluster0-1jivy.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
