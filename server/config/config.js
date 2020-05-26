305bede54cc58855e13bfaee67ecbbb325b6aea9
Fruchart Juan Alberto <jfruchart@uade.edu.ar>
2020-05-26 17:11:59 -0300

    Login normal sección 10

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,16 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //
+// Vencimiento del Token
+//
+process.env.CADUCIDAD_TOKEN = Number(60 * 60 * 24 * 30);
+
+//
+// Seed
+//
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+//
 // BD
 //
 let urlDB;
---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+
+//
+// Verificar Token
+//
+let verificaToken = (req, res, next) => {
+
+    let token = req.get('token'); // Authorization
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
+
+let verificaAdminRole = (req, res, next) => {
+
+    let usuario = req.usuario;
+
+    if (usuario.role !== 'ADMIN_ROLE') {
+        return res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es administrador'
+            }
+        });
+    } else {
+        next();
+    }
+};
+
+module.exports = {
+    verificaToken,
+    verificaAdminRole
+}
